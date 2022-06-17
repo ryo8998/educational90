732aba1cd3693a65b552cdcd02609c655ca57937
ryo8998 <ishino.r8998@gmail.com>
2022-06-17 22:43:13 +0900

    6/17 commit

---
+++ 027_2_sign_up_requests.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    unordered_map<string,int> hashmap;
+    for(int i=1; i<=N; i++){
+        string s;
+        cin >> s;
+        if(hashmap.find(s) == hashmap.end()){
+            hashmap[s] = 1;
+            cout << i << endl;
+        }
+    }
+
+
+}
