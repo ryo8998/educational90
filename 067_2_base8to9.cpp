dca3557faf35144832579cb826bc9f3c116836f4
ryo8998 <ishino.r8998@gmail.com>
2022-06-22 23:28:20 +0900

    6/22 commit

--- 067_2_base8to9.cpp
+++ 067_2_base8to9.cpp
@@ -1,9 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long to10(string N){
+    long long decimal = 0;
+    for(int i=N.size()-1; i>=0; i++){
+        decimal += N[i] * pow(8,N.size()-i-1);
+    }
+    return decimal;
+}
+
+
+
 int main(){
-    long long N;
+    string N;
     int K;
     cin >> N >> K;
+    if(N=="0"){
+        cout << 0 << endl;
+        return 0;
+    } 
+
+    vector<int> A;
+    // 10進数変換
+    long long decimal = 0;
+    int digit = 0;
+    int lastDigit;
+    long long remainder;
+    for(int i=0; i<K; i++){
+        if(i==K-1){
+            decimal = to10(N);
+            remainder = decimal;
+            while(remainder!=0){
+                A.push_back(remainder%9);
+                remainder /= 9;
+            }
+            for(int i=A.size()-1; i>=0; i--){
+            if(A[i]==8) cout << 5;
+            else cout << A[i];
+            }
+        cout << endl;
+        }else{
+            decimal = to10(N);
+            remainder = decimal;
+            while(remainder!=0){
+                A.push_back(remainder%9);
+                remainder /= 9;
+            }
+            N = "";
+            for(int i=0; i<A.size();i++){
+                if(A[i] == 8) N += '5';
+                else N += char(A[i] + '0');
+            }
+            A.clear();
+        }
+        
+    }
     
+
 }
