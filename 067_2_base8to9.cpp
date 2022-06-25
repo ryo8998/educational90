9bc301139c1a4604f455553890a63e969974d5c0
ryo8998 <ishino.r8998@gmail.com>
2022-06-25 22:40:23 +0900

    6/25 commit

--- 067_2_base8to9.cpp
+++ 067_2_base8to9.cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long to10(string N){
+long long toDecimal(string N){
     long long decimal = 0;
-    for(int i=N.size()-1; i>=0; i++){
-        decimal += N[i] * pow(8,N.size()-i-1);
+    for(int i=N.size()-1; i>=0; i--){
+        decimal += int(N[i]-'0') * pow(8,N.size()-i-1);
     }
     return decimal;
+}
+
+string toBase9(long long decimal){
+    if (decimal == 0) {
+		return "0";
+	}
+    string N = "";
+    while(decimal!=0){
+            char n = (decimal % 9 + '0');
+            n =  n == '8' ? '5' : n;
+            N = n + N;
+                decimal /= 9;
+            }
+    return N;
 }
 
 
@@ -19,42 +33,14 @@ int main(){
         cout << 0 << endl;
         return 0;
     } 
-
-    vector<int> A;
-    // 10進数変換
-    long long decimal = 0;
-    int digit = 0;
-    int lastDigit;
-    long long remainder;
     for(int i=0; i<K; i++){
-        if(i==K-1){
-            decimal = to10(N);
-            remainder = decimal;
-            while(remainder!=0){
-                A.push_back(remainder%9);
-                remainder /= 9;
-            }
-            for(int i=A.size()-1; i>=0; i--){
-            if(A[i]==8) cout << 5;
-            else cout << A[i];
-            }
-        cout << endl;
-        }else{
-            decimal = to10(N);
-            remainder = decimal;
-            while(remainder!=0){
-                A.push_back(remainder%9);
-                remainder /= 9;
-            }
-            N = "";
-            for(int i=0; i<A.size();i++){
-                if(A[i] == 8) N += '5';
-                else N += char(A[i] + '0');
-            }
-            A.clear();
-        }
-        
+        long long decimal = toDecimal(N);
+        // cout << decimal << endl;
+        N = toBase9(decimal);
     }
+    for(int i=0; i<N.size(); i++){
+        cout << N[i];
+    }
+    cout << endl;
     
-
 }
